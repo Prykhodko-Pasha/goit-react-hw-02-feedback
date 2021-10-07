120439dbc0e2539a6a777b586134110d1c941679
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-10-07 12:54:15 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
 
 export default class Feedback extends React.Component {
   state = {
@@ -7,23 +10,38 @@ export default class Feedback extends React.Component {
     bad: 0,
   };
 
+  static propTypes = {};
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
   render() {
+    let total = Object.values(this.state).reduce((a, b) => a + b);
+    let positivePercentage = Math.round((this.state.good / total) * 100);
+    const { good, neutral, bad } = this.state;
+
     return (
       <>
-        <Profile
-          name={user.name}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-
-        <Statistics title="Upload stats" stats={statisticalData} />
-        <Statistics stats={statisticalData} />
-
-        <FriendList friends={friends} />
-
-        <TransactionHistory items={transactions} />
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <p>No feedback given</p>
+          )}
+        </Section>
       </>
     );
   }
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,30 +1,28 @@
 import PropTypes from 'prop-types';
-import classes from './Statistics.module.css';
+import s from './Statistics.module.css';
 
-export default function Statistics({ title, stats }) {
+export default function Statistics({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) {
   return (
-    <section className={classes.statistics}>
-      {title && <h2 className={classes.title}>{title}</h2>}
-
-      <ul className={classes.stat}>
-        {stats.map(({ id, label, percentage }) => (
-          <li className={classes.item} key={id}>
-            <span className={classes.label}>{label}</span>
-            <span className={classes.percentage}>{percentage}</span>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul className={s.stat__list}>
+      <li className={s.stat__item}>Good: {good}</li>
+      <li className={s.stat__item}>Neutral: {neutral}</li>
+      <li className={s.stat__item}>Bad: {bad}</li>
+      <li className={s.stat__item}>Total: {total}</li>
+      <li className={s.stat__item}>Positive feedback: {positivePercentage}%</li>
+    </ul>
   );
 }
 
 Statistics.propTypes = {
-  title: PropTypes.string,
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    }),
-  ),
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
